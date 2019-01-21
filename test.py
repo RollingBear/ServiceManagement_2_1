bbcf08ce14352542a06655f6eb168c83d4582e20
RollingBear <zhangshuo1120@hotmail.com>
2019-01-21 09:09:05 +0800

    fix bug

--- test.py
+++ test.py
@@ -1,47 +1,49 @@
 # -*- coding: utf-8 -*-
 
 
-# 2019/1/10 0010 下午 4:38     
-# RollingBear
+# 2019/1/10 0010 下午 4:38
 
-import configparser
-import os
-import sys
-import time
-import threading
+__author__ = 'RollingBear'
 
+# import configparser
+# import os
+# import sys
+# import time
+# import threading
+#
 # config = configparser.ConfigParser()
 # config.read("config.ini")
 #
 # print(config.get("name", "ServiceName"))
 # print(config.get("name", "ServiceName").split(', '))
-
-Address = "Done\pack\pack"
-test = "..\..\..\pack"
-test2 = "..\..\pack"
-test3 = "..\pack"
-
-now = os.getcwd()
-print(now)
-father = os.path.abspath(os.path.dirname(now)+os.path.sep+".")
-print(father)
-grand = os.path.abspath(os.path.dirname(now)+os.path.sep+"..")
-print(grand)
-# os.popen("start " + os.path.abspath(os.path.dirname(now)+os.path.sep+"..") + Address)
-
-print(test.count("..\\"))
-print(test2.count("..\\"))
-print(test3.count("..\\"))
-print(test.replace("..\\", ""))
-print("~" * test.count("..\\") + test.replace("..\\", ""))
-Address_test = "..\..\Done\pack\pack"
-
-print("start " + os.path.abspath(
-    os.path.dirname(os.getcwd()) + os.path.sep + ("." * Address_test.count("..\\"))) + "\\" + Address_test.replace("..\\",
-                                                                                                                 ""))
-
+#
+# Address = "Done\pack\pack"
+# test = "..\..\..\pack"
+# test2 = "..\..\pack"
+# test3 = "..\pack"
+#
+# now = os.getcwd()
+# print(now)
+# father = os.path.abspath(os.path.dirname(now) + os.path.sep + ".")
+# print(father)
+# grand = os.path.abspath(os.path.dirname(now) + os.path.sep + "..")
+# print(grand)
+# # os.popen("start " + os.path.abspath(os.path.dirname(now)+os.path.sep+"..") + Address)
+#
+# print(test.count("..\\"))
+# print(test2.count("..\\"))
+# print(test3.count("..\\"))
+# print(test.replace("..\\", ""))
+# print("~" * test.count("..\\") + test.replace("..\\", ""))
+# Address_test = "..\..\Done\pack\pack"
+#
+# print("start " + os.path.abspath(
+#     os.path.dirname(os.getcwd()) + os.path.sep + ("." * Address_test.count("..\\"))) + "\\" + Address_test.replace(
+#     "..\\",
+#     ""))
+#
 # os.popen("start " + os.path.abspath(Address_test))
-
+#
 # try:
 #     _thread.start_new_thread(ReFreshThread, (myGui, ServiceNameList, 2))
 # except:
@@ -51,7 +53,7 @@ print("start " + os.path.abspath(
 # print(ServiceName)
 # SetupAddress = '..\..\\nginx-1.14.0'
 # print("sc create " + ServiceName + ' binPath="' + SetupAddress + "\\" + ServiceName('Face_', 'WinSW_') + '.exe"')
-
+#
 # import LoadConfig
 #
 # file = open(LoadConfig.loadConfig("address", "NameListAddress"), encoding="utf-8-sig")
